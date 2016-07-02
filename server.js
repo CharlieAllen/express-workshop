405ff4758b6b56b8599f772bf5cb8390e9e056fb
CharlieAllen <miss.neophyte@gmail.com>
2016-07-02 11:36:37 +0100

    middleware added

--- server.js
+++ server.js
@@ -1,21 +1,6 @@
 var express = require('express');
 var app = express();
-
-app.get("/", function(req, res){
-  res.send("Hello World");
-}); 
-
-app.get("/charlie", function(req, res){
-  res.send("Charlie is a developer");
-});
-
-app.get("/node", function(req, res){
-  res.send("This is the Node endpoint");
-});
-
-app.get("/girls", function(req, res){
-  res.send("This is the Girls endpoint");
-});
+app.use(express.static("public"));
 
 // app.listen is an inbuilt function in node
 app.listen(3000, function(){
